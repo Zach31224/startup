f4e3a109e1f46d2033806e692a4611641f499dd5
Zach31224 <zsandber@byu.edu>
2025-10-09 15:07:30 -0600

    created home folder  with the orig css file and the main elements of the html files with the proper imports and routing (based off of index.html).

---
+++ src/home/home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './home.css';
+import { NavLink } from 'react-router-dom';
+
+export function Home() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <section>
+        <h2>Welcome, User! <em>(will be changed later)</em></h2>
+        <p>
+          Pythings is a python learning website. Solve puzzles and make pixel art by writing Python code.<br />
+          Try game levels, drawing challenges, or freestyle your own art. See your code run instantly, get feedback, and share creations!
+        </p>
+        <img src="/sceetch.png" alt="Design Sketch" width="300" />
+        <ul>
+          <li><strong>Login:</strong> <NavLink to="/login">Go to Login</NavLink></li>
+          <li><strong>Editor:</strong> <NavLink to="/editor">Try coding!</NavLink></li>
+          <li><strong>Gallery:</strong> <NavLink to="/gallery">See community art</NavLink></li>
+          <li><strong>Leaderboard:</strong> <NavLink to="/scores">See scores</NavLink></li>
+        </ul>
+      </section>
+    </main>
+  );
+}
